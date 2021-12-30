98c99e1223c79305f4e142adc757e0e3e64c361a
stavalfi <stavalfi@gmail.com>
2021-12-31 00:17:43 +0200

    simplify setup of running ui and backend

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,10 +1,9 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths({ root: '../..' })],
+  plugins: [reactRefresh()],
   build: {
     outDir: 'ui-dist',
   },
